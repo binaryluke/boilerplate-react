e55536c8fd7e64eb21147138a891990591f73a63
Luke Howard <3931305+binaryluke@users.noreply.github.com>
2019-12-24 10:55:36 +1100

    Basic transpilation of es6 + jsx

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'Binaryluke - React Boilerplate'
+
+ReactDOM.render(
+  <h1>{title}</h1>,
+  document.querySelector('body')
+)
+
